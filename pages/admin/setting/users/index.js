ba409e2e81103ea7ccf1642ac14fe0cd3ecdab3f
rizkifac2204 <rizkifac2204@gmail.com>
2022-01-12 23:58:59 +0700

    sedikit bagian user, coba aplikasikan material table

--- pages/admin/setting/users/index.js
+++ pages/admin/setting/users/index.js
@@ -1,9 +1,26 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Users() {
+  const [data, setdata] = useState([]);
+
+  useEffect(() => {
+    function fetchData() {
+      axios
+        .get(`/api/setting/users`)
+        .then((res) => {
+          setdata(res.data);
+        })
+        .catch((err) => {
+          console.log(err.response);
+        });
+    }
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Halaman Manajemen User</h1>
+      {JSON.stringify(data)}
     </div>
   );
 }
@@ -15,7 +32,7 @@ Users.breadcrumb = [
     title: "Home",
   },
   {
-    path: "/admin/setting/user",
+    path: "/admin/setting/users",
     title: "Users",
   },
 ];
